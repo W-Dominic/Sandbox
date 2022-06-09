9d0133f245568dcfb13aea8ad8d33a7f1de21484
W-Dominic <dom0_o@protonmail.com>
2022-06-08 20:15:09 -0400

    simple starter project

--- React/reactbasics/src/App.js
+++ React/reactbasics/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className="App">
-      <h1>Simple React/Python App</h1>
+      <Header />
+      <Body />
     </div>
   );
 }
---
+++ React/reactbasics/src/components/Body.js
@@ -0,0 +1,13 @@
+function Body() {
+    return (
+        <div className="container">
+            <h2>List of things I want this app to do</h2>
+            <ul>
+                <li>display data from a sql server</li>
+                <li>make REST-ful API Calls to interact with the server</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Body;
