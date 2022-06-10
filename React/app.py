eed830886a33a652128228def461a8ac4914b4ce
WDominic <dom0_o@protonmail.com>
2022-06-09 22:37:55 -0400

    started

---
+++ React/app.py
@@ -0,0 +1,5 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
+
