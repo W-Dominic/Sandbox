c106a92b82dcb60c23ee483e816fff7d94ea90f6
WDominic <dom0_o@protonmail.com>
2022-08-26 18:23:35 -0400

    made a working button

---
+++ VueTutorial/vuetutorial/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+
+</script>
+
+<template>
+  <h2> Navbar Component </h2>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
