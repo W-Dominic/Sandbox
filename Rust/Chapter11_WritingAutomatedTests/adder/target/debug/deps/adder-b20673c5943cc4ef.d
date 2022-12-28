96b9e13505a6e94b333bf3bd669342035c3b4f16
W-Dominic <dom0_o@protonmail.com>
2022-12-28 09:24:07 -0500

    Chapter 11 Writing tests

--- Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-b20673c5943cc4ef.d
+++
@@ -1,7 +0,0 @@
-/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-b20673c5943cc4ef.rmeta: src/lib.rs
-
-/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/libadder-b20673c5943cc4ef.rlib: src/lib.rs
-
-/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-b20673c5943cc4ef.d: src/lib.rs
-
-src/lib.rs:
--- Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-d4ecb22049a43210.d
+++
@@ -1,5 +0,0 @@
-/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-d4ecb22049a43210: src/lib.rs
-
-/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-d4ecb22049a43210.d: src/lib.rs
-
-src/lib.rs:
