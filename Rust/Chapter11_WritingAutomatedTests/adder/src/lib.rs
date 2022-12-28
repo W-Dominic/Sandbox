fe591c71ce9bcd7c552ff547e71cef40650fafe5
W-Dominic <dom0_o@protonmail.com>
2022-12-28 09:22:08 -0500

    Rust Chapter 11 writing tests

---
+++ Rust/Chapter11_WritingAutomatedTests/adder/src/lib.rs
@@ -0,0 +1,66 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // for putting Rectangle into scope
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+    /*
+    #[test]
+    fn fails(){
+        panic!("This test fails!")
+    }
+    */
+    #[test]
+    fn test_rec(){
+        let rec0 = Rectangle{width:3, height:4};
+        let rec1 = Rectangle{width:2, height:2};
+
+        assert!(rec0.can_hold(&rec1));
+        assert!(!rec1.can_hold(&rec0));
+    }
+
+    #[test]
+    #[should_panic] //tests for panic!
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    //using if statement and Result<> to prevent panic when failure
+    #[test]
+    fn it_works_noPanic() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+}
---
+++ Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-b20673c5943cc4ef.d
@@ -0,0 +1,7 @@
+/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-b20673c5943cc4ef.rmeta: src/lib.rs
+
+/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/libadder-b20673c5943cc4ef.rlib: src/lib.rs
+
+/home/dom/Projects/Side-Projects/Rust/Chapter11_WritingAutomatedTests/adder/target/debug/deps/adder-b20673c5943cc4ef.d: src/lib.rs
+
+src/lib.rs:
