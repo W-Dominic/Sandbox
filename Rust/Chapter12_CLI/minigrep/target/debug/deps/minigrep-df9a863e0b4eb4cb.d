2ea1494e77ac95af12767594c19ba222f1aec704
WDominic <dom0_o@protonmail.com>
2022-12-30 15:35:41 -0500

    fixed

--- Rust/Chapter12_CLI/minigrep/target/debug/deps/minigrep-df9a863e0b4eb4cb.d
+++
@@ -1,5 +0,0 @@
-/home/dom/Projects/Side-Projects/Rust/Chapter12_CLI/minigrep/target/debug/deps/minigrep-df9a863e0b4eb4cb: src/main.rs
-
-/home/dom/Projects/Side-Projects/Rust/Chapter12_CLI/minigrep/target/debug/deps/minigrep-df9a863e0b4eb4cb.d: src/main.rs
-
-src/main.rs:
--- Rust/Chapter12_CLI/minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/home/dom/Projects/Side-Projects/Rust/Chapter12_CLI/minigrep/target/debug/minigrep: /home/dom/Projects/Side-Projects/Rust/Chapter12_CLI/minigrep/src/main.rs
