06cb05028e9fcaef49081f4c07ade9c99023003d
WDominic <dom0_o@protonmail.com>
2022-12-30 17:12:28 -0500

    print to stderr

--- Rust/Chapter12_CLI/minigrep/src/main.rs
+++ Rust/Chapter12_CLI/minigrep/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     // parse args
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -17,7 +17,7 @@ fn main() {
 
     // run and check for errors
     if let Err(e) = minigrep::run(config) {
-        println!("Application Logic Error: {e}");
+        eprintln!("Application Logic Error: {e}");
         process::exit(1);
     }
 }
