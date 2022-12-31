50173584e0ac76ebfe23237da4211e46405fa6b9
W-Dominic <dom0_o@protonmail.com>
2022-12-31 14:39:50 -0500

    adding funcitonal programming to cli project

--- Rust/Chapter12_CLI/minigrep/src/main.rs
+++ Rust/Chapter12_CLI/minigrep/src/main.rs
@@ -4,10 +4,10 @@ use std::process; // for exiting
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
 
     // parse args
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
