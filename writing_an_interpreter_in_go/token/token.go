f50d200bb390eae85160af8fc8ba89570bb98da6
W-Dominic <dom0_o@protonmail.com>
2023-05-25 14:55:30 -0400

    1.3 the lexer

--- writing_an_interpreter_in_go/token/token.go
+++ writing_an_interpreter_in_go/token/token.go
@@ -6,7 +6,6 @@ type Token struct {
     Type TokenType
     Literal string
 }
-
 const (
     ILLEGAL = "ILLEGAL"
     EOF = "EOF"
@@ -27,3 +26,15 @@ const (
     FUNCTION = "FUNCTION"
     LET = "LET"
 )
+
+var keywords = map[string]TokenType {
+    "fn": FUNCTION,
+    "let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
