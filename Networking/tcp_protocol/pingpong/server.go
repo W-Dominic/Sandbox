755bdc9d3387d396171aa4e10b3c393e844e2be3
W-Dominic <dom0_o@protonmail.com>
2023-06-05 16:51:10 -0400

    simple ping pong TCP

---
+++ Networking/tcp_protocol/pingpong/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "net"
+)
+func main() {
+    // Open a listener on a random port, and wait for a connection
+    listener, err := net.Listen("tcp", "127.0.0.1:")
+    if err != nil {
+        fmt.Errorf("Error: %w\n", err)
+        return
+    }
+    go func() {
+        conn, err := listener.Accept()
+        if err != nil {
+            fmt.Errorf("Error: %w\n", err)
+            return
+        }
+        defer conn.Close()
+
+        // Read any ping messages coming in
+        buf := make([]byte, 1024)
+        n, err := conn.Read(buf)
+        if err != nil {
+            fmt.Errorf("Error: %w\n", err)
+            return
+        }
+        fmt.Printf("Recieved on %s: %s\n", listener.Addr().String(), buf[:n]) 
+    }()
+    
+    // Initiate Connnection to the listner
+    conn, err := net.Dial("tcp", listener.Addr().String())
+    if err != nil {
+        fmt.Errorf("Error: %w\n", err)
+        return
+    }
+    defer conn.Close()
+    
+    // Write Ping to the Listener
+    n, err := conn.Write([]byte("Ping!!!"))
+    fmt.Printf("Sent %v bytes to %s\v\n", n, listener.Addr().String())
+    if err != nil { 
+        fmt.Errorf("Error: %w\n", err)
+        return
+    }
+
+    listener.Close()
+
+}
