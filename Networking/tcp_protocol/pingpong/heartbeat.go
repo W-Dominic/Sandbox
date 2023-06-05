0408b42fc14b2e04fa78526ba387a25bf1ae801c
W-Dominic <dom0_o@protonmail.com>
2023-06-05 18:20:32 -0400

    heartbeat

--- Networking/tcp_protocol/pingpong/heartbeat.go
+++ Networking/tcp_protocol/pingpong/heartbeat.go
@@ -2,11 +2,46 @@ package main
 
 import(
     "fmt"
-    "net"
     "time"
     "context"
     "io"
 )
+
+func main() {
+    ctx, cancel := context.WithCancel(context.Background())
+    r, w := io.Pipe()
+    done := make(chan struct{})
+    resetTimer := make(chan time.Duration, 1)
+    resetTimer <- time.Second //initial ping interval
+
+    go func() {
+        Pinger(ctx, w, resetTimer)
+        close(done)
+    }()
+
+    receivePing := func(d time.Duration, r io.Reader) {
+        if d >= 0 {
+            fmt.Printf("reseting timer (%s)\n", d)
+            resetTimer <- d
+        }
+
+        now := time.Now()
+        buf := make([]byte, 1024)
+        n, err := r.Read(buf)
+        if err != nil {
+            fmt.Println(err)
+        }
+        fmt.Printf("received %q (%s)\n", buf[:n], time.Since(now).Round(100*time.Millisecond))
+    }
+
+    for i, v := range []int64{0,200,500,0,-1,-1,-1} {
+        fmt.Printf("Run %d:\n", i)
+        receivePing(time.Duration(v)*time.Millisecond, r)
+    }
+
+    cancel()
+    <-done // ensure that the pinger exits after the context is cancelled
+}
 
 const defaultPingInterval = 10 * time.Second
 
