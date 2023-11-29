204cfad9921882dc1e249f9fbbbfec8ec185f316
W-Dominic <dom0_o@protonmail.com>
2023-11-29 18:49:28 -0500

    sensors gpu and cpu

--- Sensors/main.go
+++ Sensors/main.go
@@ -13,7 +13,8 @@ func main() {
             "message": "pong",
         })
     })
-    r.GET("/sensors", func(c *gin.Context) {
+    
+    r.GET("/sensors/", func(c *gin.Context) {
         // calls 'sensors' and saves output
         cmd := exec.Command("sensors")
         outputBytes, err := cmd.Output()
@@ -21,11 +22,43 @@ func main() {
             c.JSON(http.StatusInternalServerError, gin.H{
                 "status": "internal server error",
             })
+            return
         }
         outputString := string(outputBytes)
         c.JSON(http.StatusOK, gin.H{
             "data": outputString,
         })
     })
+
+    r.GET("/sensors/cpu", func(c *gin.Context) {
+        cmd := exec.Command("sensors", "coretemp-isa-0000")
+        outputBytes, err := cmd.Output()
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{
+                "status": "internal server error",
+            })
+            return
+        }
+        outputString := string(outputBytes)
+        c.JSON(http.StatusOK, gin.H{
+            "data": outputString,
+        })
+    })
+
+    r.GET("/sensors/gpu", func(c *gin.Context) {
+        cmd := exec.Command("sensors", "nouveau-pci-0100")
+        outputBytes, err := cmd.Output()
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{
+                "status": "internal server error",
+            })
+            return
+        }
+        outputString := string(outputBytes)
+        c.JSON(http.StatusOK, gin.H{
+            "data": outputString,
+        })
+    })
+
     http.ListenAndServe(":1337", r)
 }
